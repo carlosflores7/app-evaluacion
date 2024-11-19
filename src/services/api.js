4295e1d621b108dbe3e130b0e23ff25b07449109
carlosflores7 <cflores381@accitesz.com>
2024-11-18 18:14:54 -0600

    Front terminado - Correccion de url de api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
-  //baseURL: "https://fast-api-six-roan.vercel.app", // Nueva dirección base
+  //baseURL: "http://localhost:8000",
+  baseURL: "https://fast-api-six-roan.vercel.app", // Nueva dirección base
 });
 
 // Interceptor para incluir el token en las cabeceras de cada solicitud
